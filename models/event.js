b063ca17b64a933d21f6b4984eda62ae46376f3c
Michael Eguina <michael@corner-exit.com>
2019-07-31 18:18:56 -0700

    rough models done

--- models/event.js
+++ models/event.js
@@ -1,13 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
   var event = sequelize.define("event", {
     // Giving the event model a name of type STRING
-    name: DataTypes.STRING,
-    description: DataTypes.STRING
+    eventName: DataTypes.STRING,
+    eventDescription: DataTypes.STRING
   });
 
   event.associate = function(models) {
     // Associating Store with Posts
     // When an Store is deleted, also delete any associated Posts
+    event.belongsTo(models.organizer, {
+      foreignKey: {
+        allowNull: false
+      }
     event.belongsToMany(models.user, {
       through: models.userSignup
     });
