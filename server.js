d2247112d65f3170d1f87ad3dcfe7ba4310bab1a
Calvin Vo <calvinctvodesign@gmail.com>
2019-08-01 21:01:14 -0700

    add signup.html, html-routes to link from the front to back end for authentication

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
-var db = require("./models");
+var db = require("./models/");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-// Routes
+//Routes
 // require("./routes/event-api-routes.js")(app);
 // require("./routes/organizer-api-routes.js")(app);
 // require("./routes/user-api-routes.js")(app);
+
+app.post("/api/token", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var token = req.body.token;
+  verifyToken(token)
+})
+
+async function verifyToken(token){
+  const {OAuth2Client} = require('google-auth-library');
+  const client = new OAuth2Client(CLIENT_ID);
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    console.log( userid)
+    console.log("~~~~~~~~~~~~~~~~~~~~~~")
+    console.log(payload)
+  }
+  verify().catch(console.error);
+}
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: true }).then(function() {
@@ -21,3 +49,6 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+
+
