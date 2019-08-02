113fc88c09250ba70f93651160002c3f24c6e48b
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-08-02 15:35:43 -0700

    slight changes to handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,25 +4,25 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
 
-    app.get("/", function (req, res) {
-        // If the user already has an account send them to the members page
+    app.get("/signup", function (req, res) {
+        // If the user already has an account send them to the users page
         if (req.user) {
-            res.redirect("/members");
+            res.redirect("/users");
         }
         res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
 
     app.get("/login", function (req, res) {
-        // If the user already has an account send them to the members page
+        // If the user already has an account send them to the users page
         if (req.user) {
-            res.redirect("/members");
+            res.redirect("/users");
         }
         res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/members", isAuthenticated, function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/members.html"));
+    app.get("/users", isAuthenticated, function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/users.html"));
     });
 }
