99e1ec9394b83e100f3a60ee3ecf55b7bf5fb0b2
Michael Eguina <michael@corner-exit.com>
2019-10-14 09:18:42 -0700

    testing

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google:{
+        clientID: "966906528567-8c8bk61ta3q0l6kva3dhph6aqg9g2qqe.apps.googleusercontent.com",
+        clientSecret: "FHTB-KV6VAVdPOkNrGfpOPO4",
+        },
+        session:{
+            secret: "keyboard cat"
+        }
+}
